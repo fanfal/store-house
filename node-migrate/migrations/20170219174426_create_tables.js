725de2f55178ba6fa787a1dc9c36b80470c53bd6
Hao Xu <hxxu@thoughtworks.com>
2017-02-19 18:31:48 +0800

    Create the table build migration file.

---
+++ node-migrate/migrations/20170219174426_create_tables.js
@@ -0,0 +1,36 @@
+var PROJECT_TABLE_NAME = "test";
+
+
+var create_tables = new Migration({
+    up: function () {
+        this.execute("CREATE TABLE test (" +
+            "id INT NOT NULL AUTO_INCREMENT, " +
+            "project_name VARCHAR(30) NOT NULL, " +
+            "time_stamp DATE NOT NULL, " +
+            "PRIMARY KEY (id), " +
+            "UNIQUE (project_name)" +
+            ");");
+
+        this.create_table("project_info", function (t) {
+            t.integer('id', {auto_increment: true});
+            t.string('project_name', {limit: 30});
+            t.integer('building');
+            t.integer('unit');
+            t.integer('floor');
+            t.integer('number');
+            t.string('position');
+            t.string('type');
+            t.float('width');
+            t.float('height');
+            t.primary_key('id')
+            t.index('project_name');
+        })
+
+
+    },
+    down: function () {
+        this.drop_table("test");
+        this.drop_table("project_info");
+    }
+
+});
