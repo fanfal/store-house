5a31c2eaee5a825cf4dbfb3a50b0854403b46a29
zhangh-t <407113640@163.com>
2017-02-20 02:30:25 +0800

    add path 'node_modules' for jquery

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var db = require('./database')
+var db = require('./database');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -22,7 +22,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', index);
 app.use('/users', users);
