64d60a36ce8dfad8c89feb0bc36455de5819a724
zhangh-t <407113640@163.com>
2017-02-20 02:31:20 +0800

    remove __dirname root path

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
