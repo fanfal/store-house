ec378088d22191f7b250a6c2e0ad9fe8b9eb68cf
3D-9-6-23  张恒(10016197) <zhangh-t@glodon.com>
2017-02-27 21:27:18 +0800

    add some html css and javascript files

---
+++ public/javascripts/projectmanagement.js
@@ -0,0 +1,28 @@
+/*global $ */
+$(document).ready(function () {
+    "use strict";
+    var navLis = $(".nav-li");
+    var curIndex = -1;
+    var pages = new Array("createproject.html", "projectinformationcards.html");
+    navLis.mouseleave(function () {
+        $(this).removeClass("hovered-li");
+        $(this).css("cursor", "default");
+    });
+    
+    navLis.mouseenter(function () {
+        $(this).addClass("hovered-li");
+        $(this).css("cursor", "pointer");
+    }); 
+    
+    navLis.click (function () {
+        navLis.removeClass("selected-li");
+        $(this).addClass("selected-li");
+        var index = Number($(this).attr("value"));
+        if (curIndex != index){
+                curIndex = index;
+                $("#dynamic-frame").attr("src", "./subframes/" + pages[index - 1]) 
+            }
+    });
+    
+    $("#nav-create-li").click();
+});
