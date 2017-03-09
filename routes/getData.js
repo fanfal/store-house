e57bf1cafa240a47ecc8432d2d44a3bd2ba66208
zhangh-t <zhangh-t@glodon.com>
2017-03-09 23:25:16 +0800

    refactor projectDetails

--- routes/getData.js
+++ routes/getData.js
@@ -35,21 +35,19 @@ router.get('/projects', function (req, res, next) {
 
 })
 
-
-//前端页面分页渲染机制，必须支持该post方法
 router.post('/projectInfo', function(req, res, next) {
     try {
         if(!req.body == {}){
-               dbOperation.getProjectInfoByNameForPaggingRender(req.body.name,
-                                           req.body.cPage,
-                                           req.body.cSize
-                                           , function (data) {
+            dbOperation.getProjectInfoByNameForPaggingRender(req.body.name,
+                     req.body.cPage,
+                     req.body.cSize
+                     ,function (data) {
                    res.setHeader('Content-Type', 'application/json');
                    res.send(JSON.stringify({'data' : data, 'totals' : data.length}));
         })}
         else {
              res.setHeader('Content-Type', 'application/json');
-              res.send(JSON.stringify({'data' : {}, 'totals' : 0}));
+             res.send(JSON.stringify({'data' : {}, 'totals' : 0}));
         }
     } catch (err) {
         //TODO send error back
