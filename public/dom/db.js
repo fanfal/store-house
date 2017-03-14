57abd2ef309988b182348d573b7747908082e90b
Hao Xu <hxxu@thoughtworks.com>
2017-03-14 17:13:36 +0800

    Use node config to hanlde all config setting in our project.

--- public/dom/db.js
+++ public/dom/db.js
@@ -1,9 +1,10 @@
 var Sequelize = require('sequelize');
-exports.sequelize = new Sequelize('storehouse', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
+var config = require('config');
+exports.sequelize = new Sequelize(config.get("db.database"), config.get("db.user"), config.get("db.pass"), {
+    host: config.get("db.host"),
+    dialect: config.get("db.driver"),
     pool: {
-        max: 10,
+        max: config.get("db.maxPool"),
         min: 0,
         idle: 10000
     }
