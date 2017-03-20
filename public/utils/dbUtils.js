8522d4913f970c3bd387e2fca5cd57f7c4f2acf1
Hao Xu <hxxu@thoughtworks.com>
2017-03-20 21:13:51 +0800

    Use information of product to generate product id;

---
+++ public/utils/dbUtils.js
@@ -0,0 +1,39 @@
+const BUILDING = "栋";
+const UNIT = "单元";
+const FLOOR = "楼";
+const NUMBER = "号";
+const POSITION = "洞";
+const CM = "mm";
+const PLUS = "*";
+
+
+exports.createProductId = function (projectName, data) {
+    var productId = projectName;
+    if (data.building != null) {
+        productId += data.building + BUILDING;
+    }
+
+    if (data.unit != null) {
+        productId += data.unit + UNIT;
+    }
+
+    if (data.floor != null) {
+        productId += data.floor + FLOOR;
+    }
+
+    if (data.number != null) {
+        productId += data.number + NUMBER;
+    }
+
+    if (data.position != null) {
+        productId += data.position + POSITION;
+    }
+
+    productId += data.type;
+    productId += data.width + CM;
+    productId += PLUS + data.height + CM;
+
+    return productId;
+}
+
+
